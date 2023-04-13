4c7322679fb88962006869ad33171fe6c0d2394b
JuniorTheF <fsmart2006@gmail.com>
2023-04-13 14:40:24 +0300

    ACTIVITIES COMEBACK

---
+++ app/src/main/java/com/twit/multiplayer_test/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.twit.multiplayer_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.twit.multiplayer_test.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
