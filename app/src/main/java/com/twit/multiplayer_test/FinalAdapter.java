3ede6a2a5b5497f01d565e211b2ca57f8030da4e
Tim00tiM <olimprofl@gmail.com>
2023-06-29 21:16:38 +0300

    add cup icon to winner

--- app/src/main/java/com/twit/multiplayer_test/FinalAdapter.java
+++ app/src/main/java/com/twit/multiplayer_test/FinalAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,152 +14,24 @@ import java.util.ArrayList;
 
 public class FinalAdapter extends RecyclerView.Adapter<FinalAdapter.ViewHolder> {
     ArrayList<Member> members;
+    ArrayList<Result> results;
 
-    FinalAdapter(ArrayList<Member> members){
+    FinalAdapter(ArrayList<Member> members, ArrayList<Result> results){
         this.members = members;
+        this.results = results;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Member toFinal = members.get(position);
-        holder.name.setText(toFinal.getName()+"("+toFinal.getStats().getRole()+")");
-        String finalScore = "";
-        Integer finalScoreInteger = 0;
-        if (!toFinal.getState().getStatus().equals("dead")){
-            Integer mult = 1;
-            if (toFinal.getStats().getEnemy().equals(toFinal.getStats().getRole())){
-                mult-=1;
-            }
-            if (toFinal.getStats().getFriend().equals(toFinal.getStats().getRole())){
-                mult+=1;
-            }
-            finalScore+=(toFinal.getStats().getSurvival_bonus()*mult);
-            finalScoreInteger += toFinal.getStats().getSurvival_bonus()*mult;
-        }else{
-            finalScore+="0";
-        }
-        finalScore+="+";
-        Integer money = 0;
-        if (toFinal.getTreasures().getOpen()!=null){
-            for (Card q: toFinal.getTreasures().getOpen()){
-                if (q.getName().equals("Пачка денег")){
-                    money+=1;
-                }
+        Member toFinalMember = members.get(position);
+        Result toFinalScore = results.get(position);
+        for (Result q: results){
+            if (q.getScore()> toFinalScore.getScore()){
+                holder.kubok.setVisibility(View.GONE);
             }
         }
-        if (toFinal.getTreasures().getClose()!=null){
-            for (Card q: toFinal.getTreasures().getClose()){
-                if (q.getName().equals("Пачка денег")){
-                    money+=1;
-                }
-            }
-        }
-        if (toFinal.getStats().getRole().equals("Капитан")) {
-            finalScoreInteger += 2*money;
-            finalScore += 2*money + "+";
-        }else{
-            finalScoreInteger += money;
-            finalScore += money + "+";
-        }
-        Integer jewelery = 0;
-        if (toFinal.getTreasures().getOpen()!=null){
-            for (Card q: toFinal.getTreasures().getOpen()){
-                if (q.getName().equals("Украшения")){
-                    jewelery+=1;
-                }
-            }
-        }
-        if (toFinal.getTreasures().getClose()!=null){
-            for (Card q: toFinal.getTreasures().getClose()){
-                if (q.getName().equals("Украшения")){
-                    jewelery+=1;
-                }
-            }
-        }
-        switch (jewelery){
-            case 1:
-                jewelery = 1;
-                break;
-            case 2:
-                jewelery = 4;
-                break;
-            case 3:
-                jewelery = 8;
-                break;
-        }
-        if (toFinal.getStats().getRole().equals("Миледи")) {
-            finalScoreInteger += 2*jewelery;
-            finalScore += 2*jewelery + "+";
-        }else{
-            finalScoreInteger += jewelery;
-            finalScore += jewelery + "+";
-        }
-        Integer paintings = 0;
-        if (toFinal.getTreasures().getOpen()!=null){
-            for (Card q: toFinal.getTreasures().getOpen()){
-                if (q.getName().equals("Картина")){
-                    paintings+=1;
-                }
-            }
-        }
-        if (toFinal.getTreasures().getClose()!=null){
-            for (Card q: toFinal.getTreasures().getClose()){
-                if (q.getName().equals("Картина")){
-                    paintings+=1;
-                }
-            }
-        }
-        if (toFinal.getStats().getRole().equals("Сноб")) {
-            finalScoreInteger += 3*2*paintings;
-            finalScore += 3*2*paintings + "+";
-        }else{
-            finalScoreInteger += 3*paintings;
-            finalScore += 3*paintings + "+";
-        }
-        if (toFinal.getStats().getFriend().equals(toFinal.getStats().getRole())){
-            finalScore+="0+";
-        }
-        else{
-            for (Member q: members){
-                if (q.getStats().getRole().equals(toFinal.getStats().getFriend())){
-                    if (!q.getState().getStatus().equals("dead")){
-                        finalScore+=q.getStats().getSurvival_bonus()+"+";
-                        finalScoreInteger+=q.getStats().getSurvival_bonus();
-                    }else{
-                        finalScore+=0+"+";
-                    }
-                }
-            }
-        }
-        if (toFinal.getStats().getEnemy().equals(toFinal.getStats().getRole())){
-            finalScore+="0+";
-        }
-        else{
-            for (Member q: members){
-                if (q.getStats().getRole().equals(toFinal.getStats().getEnemy())){
-                    if (q.getState().getStatus().equals("dead")){
-                        finalScore+=q.getStats().getPower()+"+";
-                        finalScoreInteger+=q.getStats().getPower();
-                    }else{
-                        finalScore+=0+"+";
-                    }
-                }
-            }
-        }
-        Integer deathCounter = 0;
-        if (toFinal.getStats().getRole().equals(toFinal.getStats().getEnemy())){
-            for (Member q: members){
-                if (!q.getName().equals(toFinal.getName()) && !toFinal.getStats().getFriend().equals(q.getStats().getRole())){
-                    if (q.getState().getStatus().equals("dead")){
-                        deathCounter+=1;
-                    }
-                }
-            }
-        }
-        finalScoreInteger += 3*deathCounter;
-        finalScore += 3*deathCounter + "=";
-        finalScore+= finalScoreInteger;
-        holder.score.setText(finalScore);//Бонус за выживание+деньги+украшение+картина+друг+враг+психопат = результат
+        holder.name.setText(toFinalMember.getName()+"("+toFinalMember.getStats().getRole()+")");
+        holder.score.setText(toFinalScore.getCalculation());//Бонус за выживание+деньги+украшение+картина+друг+враг+психопат = результат
     }
 
     @NonNull
@@ -179,9 +52,11 @@ public class FinalAdapter extends RecyclerView.Adapter<FinalAdapter.ViewHolder> 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView name;
         TextView score;
+        ImageView kubok;
 
         ViewHolder(View view){
             super(view);
+            kubok = view.findViewById(R.id.kubok);
             name = view.findViewById(R.id.final_name);
             score = view.findViewById(R.id.score);
         }
--- app/src/main/java/com/twit/multiplayer_test/FinalOfGame.java
+++ app/src/main/java/com/twit/multiplayer_test/FinalOfGame.java
@@ -27,6 +27,7 @@ public class FinalOfGame extends AppCompatActivity {
     Lobby lobby;
     FinalAdapter finalAdapter;
     ArrayList<Member> members;
+    ArrayList<Result> results;
     private DatabaseReference mDatabase;
     SharedPreferences sp;
     String player_name;
@@ -41,20 +42,10 @@ public class FinalOfGame extends AppCompatActivity {
         player_name = sp.getString("userLogin", null) + " " + sp.getString("userId", null);
         Gson gson = new Gson();
         lobby = gson.fromJson(getIntent().getStringExtra("lobby"), Lobby.class);
-//        mDatabase.child("lobby").child("9997").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DataSnapshot> task) {
-//                lobby = task.getResult().getValue(Lobby.class);
-//                members.clear();
-//                for (Member q: lobby.getMembers().values()){
-//                    members.add(q);
-//                }
-//                finalAdapter.notifyDataSetChanged();
-//            }
-//        });
         members = new ArrayList<>(lobby.getMembers().values());
+        setResults();
         RecyclerView finalRv = findViewById(R.id.final_items_rv);
-        finalAdapter = new FinalAdapter(members);
+        finalAdapter = new FinalAdapter(members, results);
         finalRv.setAdapter(finalAdapter);
         finalRv.setLayoutManager(new LinearLayoutManager(FinalOfGame.this, LinearLayoutManager.VERTICAL, false));
         Button exit = findViewById(R.id.button_exit);
@@ -69,5 +60,173 @@ public class FinalOfGame extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void setResults() {
+        for (Member toFinal : members) {
+            String calculation = "";
+            Integer score = 0;
+            if (!toFinal.getState().getStatus().equals("dead")) {
+                Integer mult = 1;
+                if (toFinal.getStats().getEnemy().equals(toFinal.getStats().getRole())) {
+                    mult -= 1;
+                }
+                if (toFinal.getStats().getFriend().equals(toFinal.getStats().getRole())) {
+                    mult += 1;
+                }
+                calculation += (toFinal.getStats().getSurvival_bonus() * mult);
+                score += toFinal.getStats().getSurvival_bonus() * mult;
+            } else {
+                calculation += "0";
+            }
+            calculation += "+";
+            Integer money = 0;
+            if (toFinal.getTreasures().getOpen() != null) {
+                for (Card q : toFinal.getTreasures().getOpen()) {
+                    if (q.getName().equals("Пачка денег")) {
+                        money += 1;
+                    }
+                }
+            }
+            if (toFinal.getTreasures().getClose() != null) {
+                for (Card q : toFinal.getTreasures().getClose()) {
+                    if (q.getName().equals("Пачка денег")) {
+                        money += 1;
+                    }
+                }
+            }
+            if (toFinal.getStats().getRole().equals("Капитан")) {
+                score += 2 * money;
+                calculation += 2 * money + "+";
+            } else {
+                score += money;
+                calculation += money + "+";
+            }
+            Integer jewelery = 0;
+            if (toFinal.getTreasures().getOpen() != null) {
+                for (Card q : toFinal.getTreasures().getOpen()) {
+                    if (q.getName().equals("Украшения")) {
+                        jewelery += 1;
+                    }
+                }
+            }
+            if (toFinal.getTreasures().getClose() != null) {
+                for (Card q : toFinal.getTreasures().getClose()) {
+                    if (q.getName().equals("Украшения")) {
+                        jewelery += 1;
+                    }
+                }
+            }
+            switch (jewelery) {
+                case 1:
+                    jewelery = 1;
+                    break;
+                case 2:
+                    jewelery = 4;
+                    break;
+                case 3:
+                    jewelery = 8;
+                    break;
+            }
+            if (toFinal.getStats().getRole().equals("Миледи")) {
+                score += 2 * jewelery;
+                calculation += 2 * jewelery + "+";
+            } else {
+                score += jewelery;
+                calculation += jewelery + "+";
+            }
+            Integer paintings = 0;
+            if (toFinal.getTreasures().getOpen() != null) {
+                for (Card q : toFinal.getTreasures().getOpen()) {
+                    if (q.getName().equals("Картина")) {
+                        paintings += 1;
+                    }
+                }
+            }
+            if (toFinal.getTreasures().getClose() != null) {
+                for (Card q : toFinal.getTreasures().getClose()) {
+                    if (q.getName().equals("Картина")) {
+                        paintings += 1;
+                    }
+                }
+            }
+            if (toFinal.getStats().getRole().equals("Сноб")) {
+                score += 3 * 2 * paintings;
+                calculation += 3 * 2 * paintings + "+";
+            } else {
+                score += 3 * paintings;
+                calculation += 3 * paintings + "+";
+            }
+            if (toFinal.getStats().getFriend().equals(toFinal.getStats().getRole())) {
+                calculation += "0+";
+            } else {
+                for (Member q : members) {
+                    if (q.getStats().getRole().equals(toFinal.getStats().getFriend())) {
+                        if (!q.getState().getStatus().equals("dead")) {
+                            calculation += q.getStats().getSurvival_bonus() + "+";
+                            score += q.getStats().getSurvival_bonus();
+                        } else {
+                            calculation += 0 + "+";
+                        }
+                    }
+                }
+            }
+            if (toFinal.getStats().getEnemy().equals(toFinal.getStats().getRole())) {
+                calculation += "0+";
+            } else {
+                for (Member q : members) {
+                    if (q.getStats().getRole().equals(toFinal.getStats().getEnemy())) {
+                        if (q.getState().getStatus().equals("dead")) {
+                            calculation += q.getStats().getPower() + "+";
+                            score += q.getStats().getPower();
+                        } else {
+                            calculation += 0 + "+";
+                        }
+                    }
+                }
+            }
+            Integer deathCounter = 0;
+            if (toFinal.getStats().getRole().equals(toFinal.getStats().getEnemy())) {
+                for (Member q : members) {
+                    if (!q.getName().equals(toFinal.getName()) && !toFinal.getStats().getFriend().equals(q.getStats().getRole())) {
+                        if (q.getState().getStatus().equals("dead")) {
+                            deathCounter += 1;
+                        }
+                    }
+                }
+            }
+            score += 3 * deathCounter;
+            calculation += 3 * deathCounter + "=";
+            calculation += score;
+            results.add(new Result(calculation, score));
+        }
+    }
+
+
+}
+
+class Result{
+    public Result(String calculation, Integer score) {
+        this.calculation = calculation;
+        this.score = score;
+    }
+
+    String calculation;
+    Integer score;
+
+    public String getCalculation() {
+        return calculation;
+    }
+
+    public void setCalculation(String calculation) {
+        this.calculation = calculation;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
     }
 }
